34765795bb3e9ffc24330cb3b01ea79188a82549
daviruiz3 <daviruiz3@gmail.com>
2014-11-20 17:55:32 -0800

    added client side validation plugin

--- php/contact-form-handler.php
+++ php/contact-form-handler.php
@@ -3,10 +3,8 @@
 	// Validate Form Variables
 	$errors = '';
 	$myemail = 'daviruiz@outlook.com';//<-----Put Your email address here.
-	if(empty($_POST['name'])  ||
-	   empty($_POST['email']) ||
-	   empty($_POST['message']))
-	{
+
+	if(empty($_POST['name'])  || empty($_POST['email']) || empty($_POST['message'])){
 	    $errors .= "\n Error: all fields are required";
 	}
  
@@ -14,15 +12,11 @@
 	$email_address = $_POST['email'];
 	$message = $_POST['message'];
 	 
-	if (!preg_match(
-	"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
-	$email_address))
-	{
+	if (!preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", $email_address)){
 	    $errors .= "\n Error: Invalid email address";
 	}
 
 	if( empty($errors)){
-
 		$to = $myemail;
 		$email_subject = "Contact form submission: $name"; 
 		$email_body = "You have received a new message. ".
